39a4ae9a730254c4ec6d0f504877bde8bec87e80
Linus <linus.eberhard@lernende.bbw.ch>
2023-01-12 16:51:22 +0100

    initial setup

---
+++ src/main/java/ch/bbw/datavizapi/DatavizApiApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.datavizapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatavizApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatavizApiApplication.class, args);
+	}
+
+}
